d31d7e65e61845c89cf7adc7c92732eecb3f6295
TARUN BADDULA <TARUN BADDULA@Nagas>
2022-08-10 11:02:24 +0530

    commit massage

---
+++ JavaSupport/src/test/java/com/java/support/JavaSupport/JavaSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.support.JavaSupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
